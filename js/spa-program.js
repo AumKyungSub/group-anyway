ad1e6e0bd39c0da3bfa7f89f3277da676b6626f0
bambiru <dj70083798@gmail.com>
2023-07-22 18:00:43 +0900

    spa-program

--- js/spa-program.js
+++ js/spa-program.js
@@ -1,14 +1,29 @@
-let moreBtn = document.querySelectorAll('.moreBtn')[0];
+// JavaScript 코드
+let moreBtn = document.querySelectorAll('.moreBtn');
 let programsDetail = document.querySelectorAll('.programs-detail');
 
-const moreButton = () => {
-    if( moreBtn.classList.value.includes('on') ){
-        moreBtn.classList.remove('on');
-        programsDetail[0].style.display = 'none';
-    } else {
-        moreBtn.classList.add('on');
-        programsDetail[0].style.display = 'block';
+let activeIndex = -1; // 현재 열린 아이템의 인덱스
+
+const moreButton = (index) => {
+
+    if (activeIndex === index) {
+    // 클릭한 아이템이 이미 열려있는 경우
+    moreBtn[index].classList.add('on');
+    programsDetail[index].style.display = 'none';
+    activeIndex = -1;
+  } else {
+    // 클릭한 아이템이 닫혀있는 경우
+    if (activeIndex !== -1) {
+      // 이미 열린 아이템이 있는 경우 닫기
+      moreBtn[activeIndex].classList.add('on');
+      programsDetail[activeIndex].style.display = 'none';
     }
+    moreBtn[index].classList.remove('on');
+    programsDetail[index].style.display = 'block';
+    activeIndex = index;
+  }
 };
 
-moreBtn.addEventListener('click',moreButton);
+for (let i = 0; i < moreBtn.length; i++) {
+  moreBtn[i].addEventListener('click', () => moreButton(i));
+}
